463fff07df6c661b9fc76754e765db4a0e2a4a3c
Sakkar Chowdhury <sakkar.chowdhury@intellier.com>
2023-05-23 14:57:36 +0600

    array and tuple practice in typescript

---
+++ src/array.ts
@@ -0,0 +1,6 @@
+const names: string[] = ["abul", "kabul", "babul"]
+const rollNumbers: number[] =[12,123,345]
+
+const newArray = ["Abul", "Kabul", 123, true]
+
+const user: [number,string]=[234,"Mezba"] //Tuple

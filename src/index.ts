077da06d5cfcc75842315ebb77a13b298b95eb8c
Sakkar Chowdhury <sakkar.chowdhury@intellier.com>
2023-05-23 13:20:42 +0600

    primitive types practice in typescript

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,18 @@
-const course = "Next Level Web Development";
+let course = "Next Level Web Development";
+
+course = "Next Level Development";
+
+let  rollNumber = 123;
+
+rollNumber = 1234;
+
+const isAdmin = false;
 console.log("Course Name: ",course)
+
+
+//Primitive Types
+//String
+//number
+//boolean
+//null
+//undefined

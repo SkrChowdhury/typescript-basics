b8b87bbba07670477259eccf38074cdc3f11cd93
Sakkar Chowdhury <sakkar.chowdhury@intellier.com>
2023-06-11 13:28:17 +0600

    spread and rest operator

--- src/function.ts
+++ src/function.ts
@@ -1,30 +1,35 @@
-
-
 // Normal Function
-function add(num1:number,num2:number):number{
-    return num1 + num2
+// Default value
+function add(num1: number, num2: number = 10): number {
+  return num1 + num2;
 }
-add(2,3)
+add(3);
+
+//Spread Operator
+const myFriends = ['chandler', 'joey', 'ross']
+const newFriends = ['monica', 'rachel', 'pheobe']
+myFriends.push(...newFriends);
+console.log(myFriends)
 
 
+//rest Parameter
 
-const addArrow = (num1:number,num2:number):number => num1 + num2;
-addArrow(2,5)
+const greetFriends = (friend1: string,friend2: string,friend3: string):void => console.log(`Hi ${friend1}`)
 
+const addArrow = (num1: number, num2: number): number => num1 + num2;
+addArrow(2, 5);
 
-const arr =[1,4,5]
-const newArray2 = arr.map((element:number)=> element * element)
+const arr = [1, 4, 5];
+const newArray2 = arr.map((element: number) => element * element);
 
-
-
-const person : {
-    name: string;
-    balance: number;
-    addBalance(money:number): string;
+const person: {
+  name: string;
+  balance: number;
+  addBalance(money: number): string;
 } = {
-    name:"John",
-    balance:4,
-    addBalance(money:number){
-        return `My New Balance is ${this.balance+money}`
-    }
-}
+  name: "John",
+  balance: 4,
+  addBalance(money: number) {
+    return `My New Balance is ${this.balance + money}`;
+  },
+};
